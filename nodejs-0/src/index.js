1ac47485ad8a2b92d054fd29c21f20fa2ee4d7cf
Ítalo Henrique <italodssouza@gmail.com>
2020-05-11 11:15:37 -0300

    Functions Fibonacci & isFibonacci

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -1,8 +1,21 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+    let fibonacciArray = [0,1]
+    let nextValue = 0
+    let index = 1
+    while(nextValue < 350){
+        nextValue = fibonacciArray[index] + fibonacciArray[index - 1]
+        fibonacciArray.push(nextValue)
+        index += 1
+    }
+    return fibonacciArray
+}
 
-const isFibonnaci = (num) => null
+const isFibonnaci = (num) => {
+    let fibonacciArray = fibonacci()
+    return fibonacciArray.includes(num)
+}
 
 module.exports = {
     fibonacci,
